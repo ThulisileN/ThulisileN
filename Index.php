2a5670423bad0d9111c91c084c935fa6990333b7
ThulisileN <thuli@digilims.com>
2021-10-05 15:45:16 +0200

    Added form.html and Index.php

--- Index.php
+++ Index.php
@@ -1,3 +1,5 @@
-<?php
-echo "Hello World";
+<?php $name= $_POST['name'];
+        $email= $_POST['email'];
+        echo ('Your Name is:     '. $name. '<br/>');
+        echo ('Your Email is:'   . $email. '<br/>');    
 ?>
